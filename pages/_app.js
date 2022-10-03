85f4dfa4aaef1cd305d4fd0d2cb3fff86cc88c48
fsalcedo55 <fsalcedo55@hotmail.com>
2022-10-02 23:14:54 -0400

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,25 @@ import "../styles/globals.css"
 import NextNProgress from "nextjs-progressbar"
 import Layout from "../components/layout"
 import Head from "next/head"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
   return (
     <main>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      ></Script>
+      <Script strategy="lazyOnload" id="my-script">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Head>
         <link rel="icon" href="/seacraft-marine-repair.png" />
       </Head>
